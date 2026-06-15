d612fb02eb1080ce34e44a11875a348f7e16018a
yxtay <5795122+yxtay@users.noreply.github.com>
2026-06-15 12:40:50 +0800

    fix: logrotate.sh

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 LOGROTATE_STATE=${LOGROTATE_STATE:-/apps/logrotate/status}
 LOGROTATE_CONF=${LOGROTATE_CONF:-$(git -C "$(dirname "${0}")" rev-parse --show-toplevel)/logrotate/apps.conf}
+mkdir -p "$(dirname "${LOGROTATE_STATE}")"
 logrotate "${LOGROTATE_CONF}" --state "${LOGROTATE_STATE}"

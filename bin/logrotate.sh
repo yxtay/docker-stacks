7182073cc95c9d9e21116a7bf1fa5d761ba0b820
YuXuan Tay <5795122+yxtay@users.noreply.github.com>
2026-06-24 15:29:25 +0800

    fix: permissions (#175)

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 LOGROTATE_STATE=${LOGROTATE_STATE:-/apps/logrotate/status}
 LOGROTATE_CONF=${LOGROTATE_CONF:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/logrotate/apps.conf}
 mkdir -p "$(dirname "${LOGROTATE_STATE}")"
-logrotate "${LOGROTATE_CONF}" --state "${LOGROTATE_STATE}"
+sudo logrotate "${LOGROTATE_CONF}" --state "${LOGROTATE_STATE}"

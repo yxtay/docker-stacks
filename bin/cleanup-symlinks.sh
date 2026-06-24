cc2d49bd673d1a3338daceae9666447a35a3ac6f
yxtay <5795122+yxtay@users.noreply.github.com>
2026-06-24 15:28:33 +0800

    fix: permissions

--- bin/cleanup-symlinks.sh
+++ bin/cleanup-symlinks.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 for dir in /mnt/usenet /mnt/torrent; do
   if [[ -d "${dir}" ]]; then
-    find "${dir}" -xtype l -delete
+    sudo find "${dir}" -xtype l -delete
   fi
 done
--- bin/prepare-directories.sh
+++ bin/prepare-directories.sh
@@ -6,14 +6,11 @@ TARGET_USER=${SUDO_USER:-${USER}}
 dirs=(
   /apps
   /backups
-  /mnt/remote
   /mnt/remote/debrid
-  /mnt/torrent
   /mnt/torrent/downloads
   /mnt/torrent/movies
   /mnt/torrent/shows
 )
 for dir in "${dirs[@]}"; do
   mkdir -p "${dir}"
-  chown "${TARGET_USER}:${TARGET_USER}" "${dir}"
 done

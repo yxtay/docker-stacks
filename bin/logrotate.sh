a21185d372a4f40a63b579c5915a9e5c6d7c4fc9
YuXuan Tay <5795122+yxtay@users.noreply.github.com>
2026-06-28 23:59:03 +0800

    fix: logrotate (#214)

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -3,5 +3,9 @@ set -euo pipefail
 
 LOGROTATE_STATE=${LOGROTATE_STATE:-/apps/logrotate/status}
 LOGROTATE_CONF=${LOGROTATE_CONF:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/logrotate/apps.conf}
+LOGROTATE_TMP=$(mktemp /tmp/logrotate.conf.XXXXXX)
+trap 'sudo rm -f "${LOGROTATE_TMP}"' EXIT
+
 mkdir -p "$(dirname "${LOGROTATE_STATE}")"
-sudo logrotate "${LOGROTATE_CONF}" --state "${LOGROTATE_STATE}"
+sudo install -o root -g root -m 644 "${LOGROTATE_CONF}" "${LOGROTATE_TMP}"
+sudo logrotate "${LOGROTATE_TMP}" --state "${LOGROTATE_STATE}"

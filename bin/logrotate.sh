10b27d51773aea5a14c078731b178d7f1ee797d6
yxtay <5795122+yxtay@users.noreply.github.com>
2026-06-28 23:57:24 +0800

    Update logrotate.sh

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 LOGROTATE_STATE=${LOGROTATE_STATE:-/apps/logrotate/status}
 LOGROTATE_CONF=${LOGROTATE_CONF:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/logrotate/apps.conf}
-LOGROTATE_TMP=/tmp/logrotate-apps.conf
+LOGROTATE_TMP=$(mktemp /tmp/logrotate-apps.XXXXXX)
+trap 'sudo rm -f "${LOGROTATE_TMP}"' EXIT
 
 mkdir -p "$(dirname "${LOGROTATE_STATE}")"
 sudo install -o root -g root -m 644 "${LOGROTATE_CONF}" "${LOGROTATE_TMP}"
